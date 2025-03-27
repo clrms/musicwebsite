d1267ea9c27e0588d7a035a8a0e1034cf3d92342
mekk21349 <mekksilog@gmail.com>
2025-03-27 23:37:13 +0800

    Create musicHomepage.js

---
+++ musicHomepage.js
@@ -0,0 +1,60 @@
+const songsData = {
+    quickPlay: [
+        { title: "Shape of You", artist: "Ed Sheeran", img: "images/shape_of_you.jpg", src: "songs/shape_of_you.mp3" },
+        { title: "Blinding Lights", artist: "The Weeknd", img: "images/blinding_lights.jpg", src: "songs/blinding_lights.mp3" }
+    ],
+    mixedPlayer: [
+        { title: "Uptown Funk", artist: "Bruno Mars", img: "images/uptown_funk.jpg", src: "songs/uptown_funk.mp3" },
+        { title: "Senorita", artist: "Shawn Mendes & Camila Cabello", img: "images/senorita.jpg", src: "songs/senorita.mp3" }
+    ],
+    recommendedSongs: [
+        { title: "Someone Like You", artist: "Adele", img: "images/someone_like_you.jpg", src: "songs/someone_like_you.mp3" },
+        { title: "Roar", artist: "Katy Perry", img: "images/roar.jpg", src: "songs/roar.mp3" }
+    ],
+    explore: [
+        { title: "Levitating", artist: "Dua Lipa", img: "images/levitating.jpg", src: "songs/levitating.mp3" },
+        { title: "Stay", artist: "The Kid LAROI & Justin Bieber", img: "images/stay.jpg", src: "songs/stay.mp3" }
+    ],
+    categories: {
+        Favorite: [
+            { title: "Believer", artist: "Imagine Dragons", img: "images/believer.jpg", src: "songs/believer.mp3" },
+            { title: "Believer", artist: "Imagine Dragons", img: "images/believer.jpg", src: "songs/believer.mp3" },
+            { title: "Believer", artist: "Imagine Dragons", img: "images/believer.jpg", src: "songs/believer.mp3" },
+        ],
+        MostLiked: [
+            { title: "Happier", artist: "Marshmello & Bastille", img: "images/happier.jpg", src: "songs/happier.mp3" }
+        ],
+        Popular: [
+            { title: "Peaches", artist: "Justin Bieber", img: "images/peaches.jpg", src: "songs/peaches.mp3" }
+        ]
+    },
+    genres: {
+        Pop: [
+            { title: "As It Was", artist: "Harry Styles", img: "images/as_it_was.jpg", src: "songs/as_it_was.mp3" }
+        ],
+        Rock: [
+            { title: "Bohemian Rhapsody", artist: "Queen", img: "images/bohemian_rhapsody.jpg", src: "songs/bohemian_rhapsody.mp3" }
+        ],
+        HipHop: [
+            { title: "God's Plan", artist: "Drake", img: "images/gods_plan.jpg", src: "songs/gods_plan.mp3" }
+        ]
+    },
+    albums: [
+        {
+            artist: "Taylor Swift",
+            albumImg: "images/taylor_swift_album.jpg",
+            songs: [
+                { title: "Love Story", img: "images/love_story.jpg", src: "songs/love_story.mp3" },
+                { title: "You Belong With Me", img: "images/you_belong_with_me.jpg", src: "songs/you_belong_with_me.mp3" }
+            ]
+        },
+        {
+            artist: "Eminem",
+            albumImg: "images/eminem_album.jpg",
+            songs: [
+                { title: "Lose Yourself", img: "images/lose_yourself.jpg", src: "songs/lose_yourself.mp3" },
+                { title: "Without Me", img: "images/without_me.jpg", src: "songs/without_me.mp3" }
+            ]
+        }
+    ]
+};
