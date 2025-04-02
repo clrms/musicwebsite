d1182d14a884da3cc16029f3b6a0bc2d56db2196
clrms <elena.clorafel919@gmail.com>
2025-04-02 12:46:18 +0800

    Update mainPage.js

--- mainPage.js
+++ mainPage.js
@@ -145,6 +145,11 @@ document.addEventListener("DOMContentLoaded", function () {
         loadAlbums();
     });
 
+document.getElementById("aboutBtn").addEventListener("click", function() {
+        showPage("aboutPage", "AboutUs");
+        loadAlbums();
+    });
+
     // Genre & Categories Navigation
     document.getElementById("genreSelect").addEventListener("change", function() {
         if (this.value) {
